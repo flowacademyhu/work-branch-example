569cca2d770aea417b5d3e5bef9b0e399ace72da
levee98 <leventet@t-email.hu>
2020-05-16 11:52:56 +0200

    add div function

--- team1/index.js
+++ team1/index.js
@@ -4,6 +4,7 @@ const sub = (a, b) => {
   return a - b;
 };
 
+// mul
 const mul = (a, b) => {
   return a * b;
 };
@@ -12,10 +13,13 @@ const sum = (a, b) => {
   return a + b;
 };
 
+const div = (a, b) => {
+  return a / b;
+};
+
 module.exports = {
   sum: sum,
   mul: mul,
   div: div,
   sub: sub
-
 };
