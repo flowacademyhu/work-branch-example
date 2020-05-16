82d53fa3ff6e83fe7b83b594b5daa6629edb2f24
GG_Juraj <gyorgy.galfalvy@gmail.com>
2020-05-16 11:46:27 +0200

    mul and div function

--- team3/index.js
+++ team3/index.js
@@ -4,6 +4,15 @@ const sum = (a, b) => { // writen by Dani
 const sub = (a, b) => { // writen by Dani
   return a - b;
 };
+
+const mul = (a, b) => { // writen by gy
+  return a * b;
+};
+
+const div = (a, b) => { // writen by gy
+  return a / b;
+};
+
 module.exports = {
   sum,
   sub,
